60fbc05939b3c85fc50a751e4f784c45000412a8
https://github.com/Sammi198478 <farhanakanij4@gmail.com>
2025-01-15 22:01:55 +0600

    HomePage update with bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 const HomePage = () => {
   return (
     <>
-    <main className='flex justify-center items-center h-screen'>
-      <h1 className='font-bold text-5xl text-red-900'>Hello World!</h1>
+    <main className='flex justify-center items-center min-h-screen bg-sky-200'>
+      <div className='w-full max-w-screen-2xl mx-auto p-20 bg-slate-100 rounded-2xl shadow-lg'>
+      <h1 className='font-bold text-5xl text-red-900 text-center'>Hello World!</h1>
+      </div>
     </main>
     </>
   )  
